5a07bdad5de3227bd92f969231bee325004b9db0
xaiur <davinoverclock@gmail.com>
2022-12-30 12:02:38 +0800

    Flutter Bloc Consumer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,41 +9,71 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Listener"),
+        title: Text("Flutter Bloc Consumer"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BlocListener<Counter, int>(
+          BlocConsumer<Counter, int>(
             bloc: mycounter,
+            builder: (context, state) {
+              return Center(
+                child: Text(
+                  "$state",
+                  style: TextStyle(
+                    fontSize: 50,
+                  ),
+                ),
+              );
+            },
             listener: (context, state) {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text("Kelipatan 10!"),
+                  content: Text("Kelipatan 5!"),
+                  duration: Duration(milliseconds: 500),
                 ),
               );
             },
             listenWhen: (previous, current) {
-              if (current % 10 == 0) {
+              if (current % 5 == 0) {
                 return true;
               } else {
                 return false;
               }
             },
-            child: BlocBuilder<Counter, int>(
-              bloc: mycounter,
-              builder: (context, state) {
-                return Center(
-                  child: Text(
-                    "$state",
-                    style: TextStyle(
-                      fontSize: 50,
-                    ),
-                  ),
-                );
-              },
-            ),
           ),
+
+          // BlocListener<Counter, int>(
+          //   bloc: mycounter,
+          //   listener: (context, state) {
+          //     ScaffoldMessenger.of(context).showSnackBar(
+          //       SnackBar(
+          //         content: Text("Kelipatan 5!"),
+          //         duration: Duration(milliseconds: 500),
+          //       ),
+          //     );
+          //   },
+          //   listenWhen: (previous, current) {
+          //     if (current % 5 == 0) {
+          //       return true;
+          //     } else {
+          //       return false;
+          //     }
+          //   },
+          //   child: BlocBuilder<Counter, int>(
+          //     bloc: mycounter,
+          //     builder: (context, state) {
+          //       return Center(
+          //         child: Text(
+          //           "$state",
+          //           style: TextStyle(
+          //             fontSize: 50,
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
           SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
