e1905bd86bb84f10851fee1b52c8e8b2a0728c48
xaiur <davinoverclock@gmail.com>
2023-01-03 16:56:46 +0800

    Bloc Access - Named Route Access

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:latihan_bloc/bloc/counter.dart';
 import 'package:latihan_bloc/pages/home.dart';
+import 'package:latihan_bloc/pages/other.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  Counter mycounter = Counter();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => Counter(),
-        child: HomePage(),
-      ),
+      // home: BlocProvider(
+      //   create: (context) => Counter(),
+      //   child: HomePage(),
+      // ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => BlocProvider.value(
+              value: mycounter,
+              child: HomePage(),
+            ),
+        "/other": (context) => BlocProvider.value(
+              value: mycounter,
+              child: OtherPage(),
+            ),
+      },
     );
   }
 }
