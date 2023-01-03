230d531ee528fa667b0e47c683a15609f59bda02
xaiur <davinoverclock@gmail.com>
2023-01-03 19:11:31 +0800

    Bloc Access - Generated Route Access

---
+++ lib/404/404.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "404",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:latihan_bloc/bloc/counter.dart';
-import 'package:latihan_bloc/pages/home.dart';
-import 'package:latihan_bloc/pages/other.dart';
+import 'package:latihan_bloc/routes/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,27 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  Counter mycounter = Counter();
+  final router = myRouter();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: BlocProvider(
-      //   create: (context) => Counter(),
-      //   child: HomePage(),
-      // ),
-      initialRoute: "/",
-      routes: {
-        "/": (context) => BlocProvider.value(
-              value: mycounter,
-              child: HomePage(),
-            ),
-        "/other": (context) => BlocProvider.value(
-              value: mycounter,
-              child: OtherPage(),
-            ),
-      },
+      onGenerateRoute: router.onRoute,
     );
   }
 }
