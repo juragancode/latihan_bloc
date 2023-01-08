bd7f5f1750dcd1edf1c2eda213097d0712ffd475
xaiur <davinoverclock@gmail.com>
2023-01-08 14:08:12 +0800

    Extension Method - watch local

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Counter myCounter = context.read<Counter>(); // hanya dibaca 1X
-    Counter myCounter = context.watch<Counter>(); // hanya dibaca 1X
+    // Counter myCounter = context.watch<
+    //     Counter>(); // mengalami perubahan saat state juga berubah, kekurangannya merebuild semua Scaffold
 
     return Scaffold(
       appBar: AppBar(
@@ -17,21 +18,24 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "${myCounter.state}",
-              style: TextStyle(
-                fontSize: 100,
-              ),
-            ),
+            Builder(builder: (context) {
+              Counter myCounter = context.watch<Counter>();
+              return Text(
+                "${myCounter.state}",
+                style: TextStyle(
+                  fontSize: 100,
+                ),
+              );
+            }),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 IconButton(
-                  onPressed: () => myCounter.kurangData(),
+                  onPressed: () => context.read<Counter>().kurangData(),
                   icon: Icon(Icons.remove),
                 ),
                 IconButton(
-                  onPressed: () => myCounter.tambahData(),
+                  onPressed: () => context.read<Counter>().tambahData(),
                   icon: Icon(Icons.add),
                 ),
               ],
