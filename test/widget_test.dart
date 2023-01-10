c792b3708f6fec313a185e966b649bbae862010c
xaiur <davinoverclock@gmail.com>
2023-01-10 14:51:46 +0800

    Equatable

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,35 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+import 'package:equatable/equatable.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+void main() {
+  Orang orang1 = Orang("John", 35);
+  Orang orang2 = Orang("John", 35);
 
-// import 'package:latihan_bloc/main.dart';
+  print(orang1 == orang2);
+  print(orang1.hashCode);
+  print(orang2.hashCode);
+}
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
+class Orang extends Equatable {
+  String nama;
+  int umur;
+  Orang(this.nama, this.umur);
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+  @override
+  List<Object?> get props => [nama, umur];
+}
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+  //-------------- TANPA EQUATABLE --------------
+  // class Orang {
+  // String nama;
+  // int umur;
+  // Orang(this.nama, this.umur);
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  // @override
+  // int get hashCode => nama.hashCode;
+
+  // @override
+  // bool operator ==(Object other) {
+  //   return identical(this, other) ||
+  //       other is Orang && this.nama == other.nama && this.umur == other.umur;
+  // }
+
